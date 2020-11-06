91853fab4d840d1d8a1b3ce38bfe260a70bb9f0b
Palmer Turley <palmerturley34@gmail.com>
2020-11-05 18:43:18 -0800

    update more files

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,5 +1,19 @@
 def word_count(s):
-    # Your code here
+    chars = ['"', ':', ';', ',', '.', '-', '+', '=', '/', '|', '[', ']', '{', '}', '(', ')', '*', '^', '&']
+    
+    words = s.split()
+    for i in range(len(words)):
+        words[i] = words[i].strip('" : ; , . - + = / \ | [ ] { } ( ) * ^ &')
+        words[i] = words[i].lower()
+    counts = {}
+    for word in words:
+        if word in counts:
+            counts[word] += 1
+        else:
+            if word:
+                counts[word] = 1
+
+    return counts
 
 
 
@@ -8,3 +22,4 @@ if __name__ == "__main__":
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
     print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count('":;,.-+=/\\|[]{}()*^&'))
