064d3df4a05b969842682c1c4453c8cf8b999607
Palmer Turley <palmerturley34@gmail.com>
2020-11-05 20:21:00 -0800

    module 4

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,23 @@
-# Your code here
+def word_count(s):
+    chars = ['"', ':', ';', ',', '.', '-', '+', '=', '/', '|', '[', ']', '{', '}', '(', ')', '*', '^', '&']
+    
+    words = s.split()
+    for i in range(len(words)):
+        words[i] = words[i].strip('" : ; , . - + = / \ | [ ] { } ( ) * ^ &')
+        words[i] = words[i].lower()
+    counts = {}
+    for word in words:
+        if word in counts:
+            counts[word] += 1
+        else:
+            if word:
+                counts[word] = 1
 
+    return counts
+
+with open("applications\histo\robin.txt") as f:
+    words = f.read()
+words = words.split()
+counts = word_count(words)
+counts.sort()
+# unfinished
--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -1,13 +1,38 @@
 import random
 
 # Read in all the words in one go
-with open("input.txt") as f:
+with open("applications\markov\input.txt") as f:
     words = f.read()
 
+words = words.split()
+
+
 # TODO: analyze which words can follow other words
-# Your code here
+start_words = []
+stop_words = []
+follows = {}
+for i in range(len(words)-1):
+    if words[i][0] == '"' and words[i][1].isupper():
+        start_words.append(words[i])
+    if words[i][0].isupper():
+        start_words.append(words[i])
+    if words[i][-1] in ['.', '?', '!']:
+        stop_words.append(words[i])
+    if words[i][-1] == '"' and words[i][-2] in ['.', '?', '!']:
+        stop_words.append(words[i])
+    if words[i] in follows:
+        follows[words[i]].append(words[i+1])
+    else:
+        follows[words[i]] = [words[i+1]]
 
 
 # TODO: construct 5 random sentences
-# Your code here
+import random
+for _ in range(5):
+    word = random.choice(start_words)
+    while word not in stop_words:
+        print(word, end=" ")
+        word = random.choice(follows[word])
+    print(word, '\n')
+
 
